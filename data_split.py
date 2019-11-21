60f5604a2732606aef7e5aac9f063c43a9989ab3
jcremaldi <30802497+jcremaldi@users.noreply.github.com>
2019-11-21 11:23:56 -0600

    Added data adjust for time-series (M,W,D,H)

--- data_split.py
+++ data_split.py
@@ -1,9 +1,21 @@
 import pandas as pd
 
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 9999)
+
+
 raw_data = pd.read_csv(r'../New folder/train.csv')
 raw_data = raw_data[raw_data['meter_reading'] != 0]
-print(raw_data.dtypes)
-print(raw_data.groupby('building_id')['meter'].value_counts())
+
+#------------------------------------------------------------------------------
+#print(raw_data.dtypes)
+#print(raw_data.groupby('building_id')['meter'].value_counts())
+
+# data not missing for all march
+#datetime_counts = raw_data['timestamp'].value_counts().sort_index()
+#datetime_counts.to_csv('datetime_counts.csv')
+#------------------------------------------------------------------------------
 
 building_id = 1427
 meter = 2
--- test.py
+++ test.py
@@ -1,34 +1,76 @@
 import pandas as pd
+import datetime as dt
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-
-building_id = 1427
-meter = 2
+building_id = 7
+meter = 1
 raw_data = pd.read_csv(r'../New folder/%s_%s.csv' % (building_id,meter))
-raw_data = raw_data.drop(columns = ['building_id','meter'])
+raw_data = raw_data.drop(columns = ['building_id','meter','Unnamed: 0'])
 raw_data.timestamp = pd.to_datetime(raw_data.timestamp)
+raw_data = raw_data.set_index('timestamp')
 
 # account for xmas in data
-xmas_ave = raw_data[raw_data['timestamp'] > '2016-12-19 03:00:00']['meter_reading'].mean()
+xmas_ave = raw_data[raw_data.index > '2016-12-19 03:00:00']['meter_reading'].mean()
 print(xmas_ave)
-raw_data = raw_data[raw_data['timestamp'] < '2016-12-19 03:00:00']
+raw_data = raw_data[raw_data.index < '2016-12-19 03:00:00']
+# to do --> remove weekends and holidays to a separate df and just use the average
 
-#raw_data = raw_data.set_index('timestamp')
-#raw_data_weekly = raw_data.resample('W').mean()
-#rolling = raw_data.rolling(window=168)
+polyn = 4
 
-y_values = raw_data.loc[:, 'meter_reading']
-x_values = np.linspace(0,1,len(raw_data.loc[:, 'meter_reading']))
-poly_degree = 2
-coeffs = np.polyfit(x_values, y_values, poly_degree)
-print(coeffs)
-poly_eqn = np.poly1d(coeffs)
-y_hat = poly_eqn(x_values)
+raw_data['H'] = raw_data.index.hour
+raw_data['D'] = raw_data.index.weekday_name
+day_conv = {'Monday':1, 'Tuesday':2, 'Wednesday':3, 'Thursday':4, 'Friday':5, 'Saturday':6, 'Sunday':7}
+raw_data['D'] = raw_data['D'].map(day_conv)
+raw_data['W'] = raw_data.index.week
+raw_data['M'] = raw_data.index.month
 
-plt.figure(figsize=(12,8))
-plt.plot(raw_data.loc[:, 'timestamp'], raw_data.loc[:,'meter_reading'])
-plt.plot(raw_data.loc[:, 'timestamp'],y_hat)
-plt.ylabel('meter reading')
-plt.xlabel('timestamp')
+monthly = raw_data.groupby('M').meter_reading.mean().reset_index()
+trendM = np.poly1d(np.polyfit(monthly.M, monthly.meter_reading, polyn))
+trendMpoly = np.poly1d(trendM) 
+
+raw_data['mr_m_adj'] = raw_data.meter_reading - trendMpoly(raw_data.M)
+weekly = raw_data.groupby('W').mr_m_adj.mean().reset_index()
+trendW = np.poly1d(np.polyfit(weekly.W, weekly.mr_m_adj, 1))
+trendWpoly = np.poly1d(trendW) 
+
+raw_data['mr_mw_adj'] = raw_data.mr_m_adj - trendWpoly(raw_data.W)
+
+daily = raw_data.groupby('D').mr_mw_adj.mean().reset_index()
+trendD = np.poly1d(np.polyfit(daily.index+1, daily.mr_mw_adj, 1))
+trendDpoly = np.poly1d(trendD)
+
+raw_data['mr_mwd_adj'] = raw_data.mr_mw_adj - trendDpoly(raw_data.D)
+hourly = raw_data.groupby('H').mr_mwd_adj.mean().reset_index()
+trendH = np.poly1d(np.polyfit(hourly.H, hourly.mr_mwd_adj, polyn))
+trendHpoly = np.poly1d(trendH)
+
+raw_data['mr_mwdh_adj'] = raw_data.mr_mwd_adj - trendHpoly(raw_data.H)
+
+plt.plot(monthly.M, monthly.meter_reading,'.')
+plt.plot(monthly.M,trendMpoly(monthly.M))
 plt.show()
+
+plt.plot(weekly.W, weekly.mr_m_adj,'.')
+plt.plot(weekly.W,trendWpoly(weekly.W))
+plt.show()
+
+plt.plot(daily.D, daily.mr_mw_adj,'.')
+plt.plot(daily.D,trendDpoly(daily.D))
+plt.show()
+
+plt.plot(hourly.H, hourly.mr_mwd_adj,'.')
+plt.plot(hourly.H,trendHpoly(hourly.H))
+plt.show()
+
+
+
+
+# To do:
+# set full hourly yearly index
+# fill missing data values
+# separate weekend, holiday usage from normal
+# get trnds for hourly, daily, weekly, monthly and adjust data
+# --> monthly, adjust, weekly, adjust, daily, adjust, hourly, adjust
+# store the coef in a dict for each building/meter combo
+# maybe use coeff in regression, or use to adjust and back calculate
